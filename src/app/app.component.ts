f99fccc2adf39d3e37468dee53cbee39394746e2
Nelson Belgarde <nelsonbelgarde@gmail.com>
2021-12-13 12:01:33 +0100

    Rename title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'questNgApp';
+  title = 'Bienvenue sur le site de Nelson !';
 }
